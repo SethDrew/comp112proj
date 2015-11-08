f78590b96635800276690e49b7a559827a9a9e8e
Jacob Apkon <japkon12@gmail.com>
2015-11-08 18:17:33 -0500

    Change tabs to spaces in proxy.py

--- proxy.py
+++ proxy.py
@@ -1,36 +1,39 @@
 from ds import TTLDict
 
 class Proxy:
-	peers = [] # array of [(hostname, port), (hostname, port)]
+    peers = [] # array of [(hostname, port), (hostname, port)]
 
-	def __init__(self, srvname, srvport):
-		self.srvname = srvname
-		self.srvport = srvport
-		self.data = TTLDict()
+    def __init__(self, srvname, srvport):
+        self.srvname = srvname
+        self.srvport = srvport
+        self.data = TTLDict()
 
-	def add_peer(self, hostname, port): # should do some sort of pinging here
-		peers.append((hostname,port))
+    def add_peer(self, hostname, port): # should do some sort of pinging here
+        peers.append((hostname,port))
 
-	def del_peer(self, hotname, port):
-		for i, (h, p) in enumerate(peers):
-			if h == hostname and p == port:
-				peers.remove(i)
+    def del_peer(self, hotname, port):
+        for i, (h, p) in enumerate(peers):
+            if h == hostname and p == port:
+                peers.remove(i)
 
-	def ping_server(self):
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		tmp = sock.connect_ex((self.srvname, self.srvport))
-		if tmp == 0:
-			print "Connection available"
-		else:
-			print tmp
+    def ping_server(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tmp = sock.connect_ex((self.srvname, self.srvport))
+        if tmp == 0:
+            print "Connection available"
+        else:
+            print tmp
 
-	def get(self, key):
-		ret = self.data.get(key)
-		if ret == None:
-			print "Proxy does not contain key"
-		else:
-			# for proxy in the list, send a UDP question: do you contain something?
-			print ret
-			#step 1: ask peers
-			#step 2: get the file from the server
-			#do this asynchronously and provide the most recent?
+    def get(self, key):
+        ret = self.data.get(key)
+        if ret == None:
+            print "Proxy does not contain key"
+        else:
+            # for proxy in the list, send a UDP question: do you contain something?
+            print ret
+            #step 1: ask peers
+            #step 2: get the file from the server
+            #do this asynchronously and provide the most recent?
+
+    def forward(self, msg):
+        print "Forwarding:", msg
