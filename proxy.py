d355d449dafc0c8938a52a1c136019704b1ec644
Seth Drew <sethdrew@wr-130-64-193-25.medford.tufts.edu>
2015-11-17 12:33:00 -0500

    Added preliminary UDP proxy sharing logic

--- proxy.py
+++ proxy.py
@@ -8,9 +8,7 @@ WEB_SERVER_PORT = 80
 class Proxy:
     peers = [] # array of [(hostname, port), (hostname, port)]
 
-    def __init__(self, srvname, srvport):
-        self.srvname = srvname
-        self.srvport = srvport
+    def __init__(self):
         self.data = TTLDict()
 
     def add_peer(self, hostname, port): # should do some sort of pinging here
@@ -21,22 +19,24 @@ class Proxy:
             if h == hostname and p == port:
                 peers.remove(i)
 
-    def ping_server(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tmp = sock.connect_ex((self.srvname, self.srvport))
-        if tmp == 0:
-            print "Connection available"
-        else:
-            print tmp
 
-    def get(self, key):
+    """ Currently this function is broken, don't use it. """
+    def get(self, key): 
         ret = self.data.get(key)
         if ret == None:
             print "Proxy does not contain key"
-        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+            for (hostname, port) in peers:
+                print "Sending UDP message for file " + key+" to "+ " Proxy Peer: "+hostname ":"+ port
+                sock.sendto(MessageBuilder.(ProxyPeerReq(key)), (hostname, port))
+            ret, address = sock.recvfrom(4096)
+            print "PROXY:: got from peer: " + ret 
+            if ret:
+                return ret
+        return ret
+
             # for proxy in the list, send a UDP question: do you contain something?
             # implement multicasting
-            print ret
             #step 1: ask peers
             #step 2: get the file from the server
             #do this asynchronously and provide the most recent?
@@ -45,6 +45,7 @@ class Proxy:
         key = msg.split("\n")[0].split(" ")[1]
         if self.data.get(key) != None:
             return "PROXY:: found data:" + self.data.get(key)
+            
         host = [x.split()[1] for x in msg.splitlines() if x.startswith("Host:")][0]
         forward_socket = socket.create_connection((host, WEB_SERVER_PORT))
         forward_socket.send(msg)
